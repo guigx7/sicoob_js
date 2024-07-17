eb9305247e00b3ba9c6b4068ed09bf43efdf06ef
guigx <guigo211103@gmail.com>
2024-07-17 12:33:39 -0300

    dropdown function added

--- script.js
+++ script.js
@@ -3,3 +3,39 @@ function copyToClipboard(elementId) {
   copyText.select();
   document.execCommand("copy");
 }
+
+
+function populateDropdown() {
+  console.log(document.getElementById('Caminho').value);
+    // Assume the variable Caminho is provided with values separated by commas
+    var caminho = document.getElementById('Caminho').value; // Replace this with the actual dynamic value source
+    var caminhoArray = caminho.split(',');
+
+    var select = document.getElementById('navegacaoURA');
+
+    // Clear any existing options
+    select.innerHTML = '';
+
+    // Add the first static option
+    var defaultOption = document.createElement('option');
+    defaultOption.value = "";
+    defaultOption.text = "Navegação:";
+    defaultOption.selected = true;
+    defaultOption.disabled = true;
+    defaultOption.style.color = "#000000"
+    select.appendChild(defaultOption);
+
+    // Populate the dropdown with the array values
+    caminhoArray.forEach(function(item) {
+        var option = document.createElement('option');
+        option.value = item.trim();
+        option.text = item.trim();
+        option.disabled = true;
+        option.style.color = "#000000"
+        select.appendChild(option);
+    });
+}
+
+window.onload = function() {
+    populateDropdown();
+}
